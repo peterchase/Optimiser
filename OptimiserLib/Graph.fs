b692b03d21421f1574d18dc1b23247ec1fc5b576
Peter Chase <peter.chase@grantadesign.com>
2015-01-13 08:26:03 +0000

    Allow for exceptions during charting

--- OptimiserLib/Graph.fs
+++ OptimiserLib/Graph.fs
@@ -24,22 +24,24 @@ module Graph =
 
             let curve = graphPointIndexes |> Seq.map (fun i -> x i) |> Seq.map (fun x -> (x, f x))
             Success curve
-
         with
             | ex -> Failure ex.Message
 
     let private stepsCurve history =
-        let curve = history |> List.map (fun sd -> (sd.x, sd.valueAndDerivatives.value))
-        curve
+        history |> List.map (fun sd -> (sd.x, sd.valueAndDerivatives.value))
         
     let private combinedLineChart (solution: seq<double * double>) (func: seq<double * double>) =
+        try
             let line1 = Chart.Line(func, Name="Function")
             let line2 = Chart.Line(solution, Name="Solution").WithMarkers(Size=10, Style=ChartTypes.MarkerStyle.Circle)
             let lines = [ line1; line2 ]
             let xGraphMin = Seq.head func |> fst |> floor
             let xGraphMax = Seq.last func |> fst |> ceil
-            (Chart.Combine lines).WithTitle("Solving by Newton's Method").WithXAxis(Title="X", Min=xGraphMin, Max=xGraphMax).WithYAxis(Title="F(X)")
+            let combined = (Chart.Combine lines).WithTitle("Solving by Newton's Method").WithXAxis(Title="X", Min=xGraphMin, Max=xGraphMax).WithYAxis(Title="F(X)")
+            Success combined
+        with
+            | ex -> Failure ex.Message
 
     let doGraph settings f history =
         let solution = stepsCurve history
-        functionCurve settings f history |> TwoTrack.Binding.bindSimple (combinedLineChart solution)
+        functionCurve settings f history |> TwoTrack.Binding.bindTwoTrack (combinedLineChart solution)
