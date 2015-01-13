66bfc0764872a03956aea3e707d781dc2105f827
Peter Chase <peter.chase@grantadesign.com>
2015-01-13 13:26:06 +0000

    Issue #3 Seems to work better if one bails out of step reduction at step threshold

--- OptimiserLib/Step.fs
+++ OptimiserLib/Step.fs
@@ -17,22 +17,19 @@ module Step =
         let expectedChangeDirection = derivatives.first * adjustedStep |> sign
         let actualChangeDirection = deltaFx |> sign
         if expectedChangeDirection = actualChangeDirection then
-            Success adjustedStep
+            adjustedStep
         else if stepFactor > settings.minStepFactor then
             let reducedStepFactor = stepFactor * settings.stepReductionFactor
             calcSafeStep settings derivatives f x reducedStepFactor
         else
-            Failure "Unable to find a safe step. Is the objective function discontinuous?"
+            adjustedStep
 
     let private calcStep settings f x derivatives =
         let tol = settings.zeroDerivativeTolerance
         try
             match derivatives with
                 | d when abs d.first < tol -> Success { x = x; step = None; valueAndDerivatives = derivatives }
-                | d -> let safeStepOrFailure = calcSafeStep settings d f x 1.0
-                       match safeStepOrFailure with
-                       | Success safeStep -> Success { x = x; step = Some safeStep; valueAndDerivatives = derivatives }
-                       | Failure f -> Failure f
+                | d -> Success { x = x; step = Some (calcSafeStep settings d f x 1.0); valueAndDerivatives = derivatives }
         with
             | _ -> Failure "Could not calculate step"
 
